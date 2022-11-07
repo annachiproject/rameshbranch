27f684401e270bc71eb239e6aba7fd987825fba2
annachiproject <117632907+annachiproject@users.noreply.github.com>
2022-11-07 14:07:22 +0530

    Update Ramesh143Crush.java

--- Ramesh143Crush.java
+++ Ramesh143Crush.java
@@ -1,8 +1,8 @@
 public class Ramesh143Crush
 {
 	public static void main( String args[] ){
-	String ThenmozhiDevi = "girl";
-	String Rameshu = "boy";
-	String Thenmozhi_Rameshu= ThenmozhiDevi + Rameshu;
+	private String ThenmozhiDevi = "girl";
+	private String Rameshu = "boy";
+	private String Jolly = ThenmozhiDevi + Rameshu;
 	}
 }
